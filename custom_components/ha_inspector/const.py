d3361f52b285d0a0be26c6b8c3152b0a085dc390
kabrarroka <crtvesa@gmail.com>
2026-09-02 09:42:02 +0200

    Prepare HA Inspector 1.4.1 release

--- custom_components/ha_inspector/const.py
+++ custom_components/ha_inspector/const.py
@@ -8,7 +8,7 @@ from homeassistant.const import Platform
 
 DOMAIN: Final = "ha_inspector"
 NAME: Final = "HA Inspector"
-VERSION: Final = "1.4.0"
+VERSION: Final = "1.4.1"
 
 PLATFORMS: Final = (Platform.SENSOR,)
 
